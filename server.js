198e0f4b31b7e7d63f995ff94ba9834e385ae776
Josh Pomerenke <jtpomerenke@gmail.com>
2021-11-11 23:31:04 -0600

    fix cors

--- server.js
+++ server.js
@@ -20,7 +20,25 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 const db = require("./back-end/config/keys").mongoURI;
 mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
